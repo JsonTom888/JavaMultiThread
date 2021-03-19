a7f53104c92e0016682a02b63e3f3aa3c351c39e
JsonTom888 <Good666888@163.com>
2021-03-19 21:00:37 +0800

    初始化项目

---
+++ javamultithread/src/main/java/com/example/javamultithread/JavamultithreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamultithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamultithreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavamultithreadApplication.class, args);
+    }
+
+}
