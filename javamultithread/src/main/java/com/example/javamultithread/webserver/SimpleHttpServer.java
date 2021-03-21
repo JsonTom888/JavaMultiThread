1c0c4843d69015f30447377ea5364062cf28ab66
JsonTom888 <Good666888@163.com>
2021-03-21 21:56:09 +0800

    基于线程池技术实现简单web服务器

---
+++ javamultithread/src/main/java/com/example/javamultithread/webserver/SimpleHttpServer.java
@@ -0,0 +1,134 @@
+package com.example.javamultithread.webserver;
+
+import com.example.javamultithread.threadpool.DefaultThreadPool;
+import com.example.javamultithread.threadpool.ThreadPool;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 基于线程池技术的简单web服务器
+ *
+ * @author tom
+ * @version V1.0
+ * @date 2021/3/21 20:53
+ */
+public class SimpleHttpServer {
+    // 处理HttpRequest的线程池
+    static ThreadPool<HttpRequestHandler> threadPool =
+            new DefaultThreadPool<HttpRequestHandler>(1);
+    // SimpleHttpServer 的根路径
+    static String basePath;
+    static ServerSocket serverSocket;
+    // 服务监听端口
+    static int port = 8080;
+
+    public static void setPort(int port){
+        if (port > 0){
+            SimpleHttpServer.port = port;
+        }
+    }
+
+    public static void setBasePath(String basePath){
+        if (basePath != null && new File(basePath).exists() && new File(basePath).isDirectory()){
+            SimpleHttpServer.basePath = basePath;
+        }
+    }
+
+    // 启动服务器
+    public static void start() throws IOException {
+        serverSocket = new ServerSocket(port);
+        Socket socket = null;
+        while ((socket = serverSocket.accept()) != null){
+            // 接收一个客户端Socket,生成一个HttpRequestHandler,放入线程池执行
+            threadPool.execute(new HttpRequestHandler(socket));
+        }
+        serverSocket.close();
+    }
+
+    static class HttpRequestHandler implements Runnable{
+        private Socket socket;
+
+        public HttpRequestHandler(Socket socket){
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            String line = null;
+            BufferedReader br = null;
+            BufferedReader reader = null;
+            PrintWriter out = null;
+            InputStream in = null;
+
+            try {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String header = reader.readLine();
+                // 由相对路径计算出绝对路径
+                String filePath = basePath +header.split(" ")[1];
+                out = new PrintWriter(socket.getOutputStream());
+                // 如果请求资源的后缀为jpg或者ico，则读取资源并输出
+                if (filePath.endsWith("jpg") || filePath.endsWith("ico")){
+                    in = new FileInputStream(filePath);
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    int i = 0;
+                    while ((i = in.read()) != -1){
+                        baos.write(i);
+                    }
+                    byte[] array = baos.toByteArray();
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Server:Molly");
+                    out.println("Content-Type: image/jpeg");
+                    out.println("Content-Length: " + array.length);
+                    out.println("");
+                    socket.getOutputStream().write(array,0,array.length);
+                } else {
+                    br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+                    out = new PrintWriter(socket.getOutputStream());
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Server:Molly");
+                    out.println("Content-Type: text/html;charset=UTF-8");
+                    out.println("");
+                    while ((line = br.readLine()) != null){
+                        out.println(line);
+                    }
+                }
+                out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+                out.println("HTTP/1.1 500");
+                out.println("");
+                out.flush();
+            } finally {
+                close(br,in,reader,out,socket);
+            }
+        }
+    }
+
+    // 关闭socket或者流
+    private static void close(Closeable... closeables){
+        if (closeables != null){
+            for (Closeable closeable : closeables){
+                try {
+                    closeable.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        SimpleHttpServer.setBasePath("F:\\javaProject\\githubProject\\JavaMultiThread\\javamultithread\\src\\main\\resources");
+        SimpleHttpServer.start();
+    }
+}
